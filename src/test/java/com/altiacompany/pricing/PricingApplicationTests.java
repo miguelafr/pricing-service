2183aae70bd1b256b3eed70fdf9369dba41a3c39
miguelafr <miguel.francisco@n3team.com>
2026-07-21 21:32:52 +0200

    Initial version with archetype downloaded from Spring Initializr

---
+++ src/test/java/com/altiacompany/pricing/PricingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altiacompany.pricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
