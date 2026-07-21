d500f4ceb9cadadb2c8006e25b19cb92d25e1d25
miguelafr <miguel.francisco@n3team.com>
2026-07-21 21:47:17 +0200

    Code formatting

--- src/main/java/com/altiacompany/pricing/PricingApplication.java
+++ src/main/java/com/altiacompany/pricing/PricingApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PricingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PricingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PricingApplication.class, args);
+    }
 
 }
--- src/test/java/com/altiacompany/pricing/PricingApplicationTests.java
+++ src/test/java/com/altiacompany/pricing/PricingApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PricingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
