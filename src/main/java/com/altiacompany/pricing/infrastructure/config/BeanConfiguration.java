5b076db42c4b947eb5c516e7c0020c7519d2354e
miguelafr <miguel.francisco@n3team.com>
2026-07-22 09:36:28 +0200

    Added OpenAPI config for Swagger UI

--- src/main/java/com/altiacompany/pricing/infrastructure/config/BeanConfiguration.java
+++ src/main/java/com/altiacompany/pricing/infrastructure/config/BeanConfiguration.java
@@ -1,5 +1,6 @@
 package com.altiacompany.pricing.infrastructure.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,6 +8,9 @@ import com.altiacompany.pricing.application.port.input.GetPriceUseCase;
 import com.altiacompany.pricing.application.port.output.PriceRepository;
 import com.altiacompany.pricing.application.service.GetPriceService;
 import com.altiacompany.pricing.domain.service.PriceSelector;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
 public class BeanConfiguration {
@@ -22,4 +26,17 @@ public class BeanConfiguration {
         return new GetPriceService(priceRepository, priceSelector);
     }
 
+    @Bean
+    public OpenAPI customOpenAPI(
+            @Value("${info.app.name:}") String name,
+            @Value("${info.app.description:}") String description,
+            @Value("${info.app.version:}") String version) {
+        // @formatter:off
+        return new OpenAPI()
+                .info(new Info()
+                        .title(name)
+                        .description(description)
+                        .version(version));
+        // @formatter:on
+    }
 }
